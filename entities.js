4fa278080c68abf1fd87adf27b96d248792906f4
gaboy1977 <81317643+gaboy1977@users.noreply.github.com>
2025-10-04 13:05:24 -0500

    Add files via upload

---
+++ entities.js
@@ -0,0 +1,19 @@
+import { base44 } from './base44Client';
+
+
+export const Customer = base44.entities.Customer;
+
+export const TireOrder = base44.entities.TireOrder;
+
+export const AgendaItem = base44.entities.AgendaItem;
+
+export const SalesCall = base44.entities.SalesCall;
+
+export const Tire = base44.entities.Tire;
+
+export const DailyReport = base44.entities.DailyReport;
+
+
+
+// auth sdk:
+export const User = base44.auth;
